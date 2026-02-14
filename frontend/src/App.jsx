170b440f4d7d44f0dbdad33cf430238c21cba94b
venkateswarareddychalla <venkateswarareddychalla6@gmail.com>
2026-02-14 12:06:22 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Add from "./pages/Add/Add";
 import Update from "./pages/Update/Update";
 
-// Protected Route wrapper
-function PrivateRoute({ children, token }) {
-  return token ? children : <Navigate to="/login" />;
+// Simple Protected Route
+function PrivateRoute({ element }) {
+  const token = localStorage.getItem("token");
+  return token ? element : <Navigate to="/login" replace />;
 }
 
 export default function App() {
-  const [token, setToken] = React.useState(localStorage.getItem("token") || null);
+  const [token, setToken] = React.useState(() =>
+    localStorage.getItem("token")
+  );
 
   const handleLogin = (newToken) => {
     localStorage.setItem("token", newToken);
@@ -26,40 +30,37 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login Page */}
-        <Route path="/login" element={token ? <Home onLogout={handleLogout} />: <Login onLogin={handleLogin} />} />
+        {/* Login */}
+        <Route
+          path="/login"
+          element={
+            token ? (
+              <Navigate to="/" replace />
+            ) : (
+              <Login onLogin={handleLogin} />
+            )
+          }
+        />
 
-        {/* Home Page */}
+        {/* Protected Routes */}
         <Route
           path="/"
           element={
-            <PrivateRoute token={token}>
-              <Home onLogout={handleLogout} />
-            </PrivateRoute>
+            <PrivateRoute element={<Home onLogout={handleLogout} />} />
           }
         />
 
-        {/* Add Task */}
         <Route
           path="/add"
-          element={
-            <PrivateRoute token={token}>
-              <Add />
-            </PrivateRoute>
-          }
+          element={<PrivateRoute element={<Add />} />}
         />
 
-        {/* Update Task */}
         <Route
           path="/update/:id"
-          element={
-            <PrivateRoute token={token}>
-              <Update />
-            </PrivateRoute>
-          }
+          element={<PrivateRoute element={<Update />} />}
         />
 
-        {/* Fallback route */}
+        {/* Fallback */}
         <Route
           path="*"
           element={<Navigate to={token ? "/" : "/login"} replace />}
