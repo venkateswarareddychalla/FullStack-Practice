3b726b173be2854c8d286007e0b1e951530a0891
venkateswarareddychalla <venkateswarareddychalla6@gmail.com>
2026-02-14 12:11:12 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Add from "./pages/Add/Add";
 import Update from "./pages/Update/Update";
 
-// Simple Protected Route
-function PrivateRoute({ element }) {
+// Protected Route wrapper
+function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
-  return token ? element : <Navigate to="/login" replace />;
+  return token ? children : <Navigate to="/login" replace />;
 }
 
 export default function App() {
@@ -30,7 +29,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login */}
+        {/* Login Page */}
         <Route
           path="/login"
           element={
@@ -42,25 +41,37 @@ export default function App() {
           }
         />
 
-        {/* Protected Routes */}
+        {/* Home Page */}
         <Route
           path="/"
           element={
-            <PrivateRoute element={<Home onLogout={handleLogout} />} />
+            <PrivateRoute>
+              <Home onLogout={handleLogout} />
+            </PrivateRoute>
           }
         />
 
+        {/* Add Task */}
         <Route
           path="/add"
-          element={<PrivateRoute element={<Add />} />}
+          element={
+            <PrivateRoute>
+              <Add />
+            </PrivateRoute>
+          }
         />
 
+        {/* Update Task */}
         <Route
           path="/update/:id"
-          element={<PrivateRoute element={<Update />} />}
+          element={
+            <PrivateRoute>
+              <Update />
+            </PrivateRoute>
+          }
         />
 
-        {/* Fallback */}
+        {/* Fallback route */}
         <Route
           path="*"
           element={<Navigate to={token ? "/" : "/login"} replace />}
